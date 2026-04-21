f18bcfea9aeabf355a90b2c3143ba8506f1a96f2
Josephine250 <niyompanojosephine@gmail.com>
2026-04-21 19:27:44 +0200

    desktop application

--- target/classes/db-fix.sql
+++
@@ -1,4 +0,0 @@
--- Fix legacy 'password' column on students table.
--- The column existed from an old schema but is not used (auth is via the users table).
--- Make it nullable so new student inserts don't fail.
-ALTER TABLE students MODIFY COLUMN password VARCHAR(255) NULL DEFAULT NULL;
